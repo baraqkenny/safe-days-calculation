aa4f38c98f410ba17bd1c3a18791868cdcba6c38
kehinde yusuf <yusufkehindemubaraq@gmail.com>
2023-08-13 12:02:30 +0100

    I finished building the User Interface of the website

---
+++ app.js
@@ -0,0 +1,30 @@
+// function validateSelect() {
+//   const selectedInput = document.getElementById("selected-input");
+//   const selectError = document.getElementById("select-error");
+
+//   // Check if the selected option's value is empty (default)
+//   selectedInput.addEventListener('click', ()=> {
+//     if (selectedInput.value === "") {
+//     selectError.textContent = "Please select an option.";
+//   } else {
+//     selectError.textContent = "";
+//   }
+//   })
+  
+// }
+
+// validateSelect()
+
+// Submit Button Validation
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    
+    // Date Validation Field
+    const dateInput = document.querySelector('.date-input');
+    dateInput.addEventListener('input', ()=> {
+
+    })
+    
+    
+})
