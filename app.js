577b55186ebe4334cd64b8d262fa9dcfd94be035
kehinde yusuf <yusufkehindemubaraq@gmail.com>
2023-08-14 22:36:41 +0100

    Completed the functionalities to calculate the safe and unsafe days.

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-// function validateSelect() {
-//   const selectedInput = document.getElementById("selected-input");
-//   const selectError = document.getElementById("select-error");
+// variable declaration
+const dateValidation = document.getElementById("date-input");
+const selectedInput = document.getElementById("selected-input");
+const selectedInputTwo = document.getElementById("selected-input-two");
 
-//   // Check if the selected option's value is empty (default)
-//   selectedInput.addEventListener('click', ()=> {
-//     if (selectedInput.value === "") {
-//     selectError.textContent = "Please select an option.";
-//   } else {
-//     selectError.textContent = "";
-//   }
-//   })
-  
-// }
+// Get The Value of the Date Input Field
+dateValidation.addEventListener("input", () => {
+  const dateValidationField = dateValidation.value;
+  return dateValidationField;
+});
 
-// validateSelect()
+// Get The Value of the Short Cycle Option
+selectedInput.addEventListener("change", () => {
+  const selectedInputField = selectedInput.value;
+  return selectedInputField;
+});
 
-// Submit Button Validation
-const submitBtn = document.getElementById('submit-btn');
-submitBtn.addEventListener('click', (e)=> {
-    e.preventDefault();
-    
-    // Date Validation Field
-    const dateInput = document.querySelector('.date-input');
-    dateInput.addEventListener('input', ()=> {
+// Get The Value of The Long Cycle Option
+selectedInputTwo.addEventListener("change", () => {
+  const selectedInputFieldTwo = selectedInputTwo.value;
+  return selectedInputFieldTwo;
+});
 
-    })
-    
-    
-})
+//Get The Result on Button Click
+
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", validateField);
+
+function validateField(e) {
+      e.preventDefault();
+  // Check if the Date, Short Cycle and Long Cycle Field is Not Empty
+    if(dateValidation.value  !== "" && selectedInput.value !== "" && selectedInputTwo.value !== ""){
+      const date = new Date(dateValidation.value);
+      const shortCycle = new Date(date);
+      const longCycle = new Date(date);
+
+      shortCycle.setDate(shortCycle.getDate() + (selectedInput.value - 18));
+      longCycle.setDate(longCycle.getDate() + (selectedInputTwo.value - 10));
+
+      const unsafeDays = Math.abs((longCycle - shortCycle) / (1000 * 60 * 60 * 24));
+      const safeDays = 30 - unsafeDays;
+
+      console.log(`Unsafe Days: ${unsafeDays}`);
+      console.log(`Safe Days: ${safeDays}`);
+      const resultBox = document.querySelector('.result-box');
+      resultBox.style.display = 'block';
+      resultBox.innerHTML += ` 
+                              <p>Unsafe Days: ${unsafeDays}</p>   
+                              <p>Safe Days: ${safeDays}</p>    
+                            `;
+    }
+}
+   
+
+
+
